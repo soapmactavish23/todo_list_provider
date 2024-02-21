bfe4aa083b3ec3d710ee0af6c1e6b7eec9f55e98
Henrick <ricknogueira1231@gmail.com>
2024-02-21 18:44:18 -0300

    Configurando as Migrations

--- lib/app/core/database/sqlite_connection_factory.dart
+++ lib/app/core/database/sqlite_connection_factory.dart
@@ -1,6 +1,7 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:synchronized/synchronized.dart';
+import 'package:todo_list_provider/app/core/database/sqlite_migration_factory.dart';
 
 class SqliteConnectionFactory {
   static const _VERSION = 1;
@@ -12,8 +13,11 @@ class SqliteConnectionFactory {
   final _lock = Lock();
 
   SqliteConnectionFactory._();
-  static SqliteConnectionFactory get instance =>
-      _instance ??= SqliteConnectionFactory._();
+
+  factory SqliteConnectionFactory() {
+    _instance ??= SqliteConnectionFactory._();
+    return _instance!;
+  }
 
   Future<Database> openConnection() async {
     var databasePath = await getDatabasesPath();
@@ -38,8 +42,33 @@ class SqliteConnectionFactory {
     _db = null;
   }
 
-  Future<void> _onConfigure(Database db) async {}
-  Future<void> _onCreate(Database db, int version) async {}
-  Future<void> _onUpgrade(Database db, int oldVersion, int version) async {}
+  Future<void> _onConfigure(Database db) async {
+    await db.execute('PRAGMA foreign_keys = ON');
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    final batch = db.batch();
+
+    final migrations = SqliteMigrationFactory().getCreateMigration();
+
+    for (var migration in migrations) {
+      migration.create(batch);
+    }
+
+    batch.commit();
+  }
+
+  Future<void> _onUpgrade(Database db, int oldVersion, int version) async {
+    final batch = db.batch();
+
+    final migrations = SqliteMigrationFactory().getUpgradeMigration(oldVersion);
+
+    for (var migration in migrations) {
+      migration.update(batch);
+    }
+
+    batch.commit();
+  }
+
   Future<void> _onDowngrade(Database db, int oldVersion, int version) async {}
 }
