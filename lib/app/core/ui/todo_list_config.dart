f9d8e3f83864061305da020d8e4e32836243b688
soapmactavish23 <ricknogueira1231@gmail.com>
2024-02-29 22:52:24 -0300

    Construindo layout de tela de cadastro

--- lib/app/core/ui/todo_list_config.dart
+++ lib/app/core/ui/todo_list_config.dart
@@ -8,6 +8,10 @@ class TodoListConfig {
         textTheme: GoogleFonts.mandaliTextTheme(),
         primaryColor: const Color(0xff5C77CE),
         primaryColorLight: const Color(0xffABC8F7),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xff5C77CE),
+          titleTextStyle: TextStyle(fontWeight: FontWeight.bold),
+        ),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
             backgroundColor: const Color(0xff5C77CE),
