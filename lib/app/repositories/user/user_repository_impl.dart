f43f1b87e6abd2d2bb4275869cfdac7eee0f4c9b
soapmactavish23 <ricknogueira1231@gmail.com>
2024-03-02 20:21:52 -0300

    Construindo Repository e Service de usuário

---
+++ lib/app/repositories/user/user_repository_impl.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:todo_list_provider/app/exception/auth_exception.dart';
+
+import 'package:todo_list_provider/app/repositories/user/user_repository.dart';
+
+class UserRepositoryImpl implements UserRepository {
+  final FirebaseAuth _firebaseAuth;
+  UserRepositoryImpl({
+    required FirebaseAuth firebaseAuth,
+  }) : _firebaseAuth = firebaseAuth;
+
+  @override
+  Future<User?> register(String email, String password) async {
+    try {
+      final userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return userCredential.user;
+    } on FirebaseAuthException catch (e, s) {
+      log('Erro ao registrar usuário', error: e, stackTrace: s);
+      if (e.code == 'email-already-in-use') {
+        final loginTypes =
+            await _firebaseAuth.fetchSignInMethodsForEmail(email);
+        if (loginTypes.contains('password')) {
+          throw AuthException(
+              message: 'E-mail já utilizado, por favor escolha outro e-mail');
+        } else {
+          throw AuthException(
+            message:
+                'Você se cadastrou no TodoList pelo Google, por favor utilize ele para entrar',
+          );
+        }
+      } else {
+        throw AuthException(message: e.message ?? 'Erro ao registrar usuário');
+      }
+    }
+  }
+}
---
+++ lib/app/services/user/user_service_impl.dart
@@ -0,0 +1,16 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:todo_list_provider/app/repositories/user/user_repository.dart';
+import 'package:todo_list_provider/app/services/user/user_service.dart';
+
+class UserServiceImpl implements UserService {
+  final UserRepository _userRepository;
+  UserServiceImpl({
+    required userRepository,
+  }) : _userRepository = userRepository;
+
+  @override
+  Future<User?> register(String email, String password) =>
+      _userRepository.register(email, password);
+}
